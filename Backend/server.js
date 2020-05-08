6dfa9e4a68f758516712fcf96a3ccf92d0d399a0
swiftbeagle <hank.j.wu@gmail.com>
2020-05-07 21:08:22 -0400

    Backend Development

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,30 @@ const students = require('./routes/students.jss');
 const instructors = require('./routes/instructors.js');
 const classes = require('./routes/classes.js');
 
+const uri = 'mongodb://localhost/test';
+
+var MongoClient = require('mongodb').MongoClient;
+var connection = undefined;
+var mongoClientOpts = {};
+
+app.use(function expressMongoDb(req, res, next) {
+  if (!connection) {
+    connection = MongoClient.connect(uri, mongoClientOpts);
+  }
+
+  connection
+    .then(function (db) {
+      req.db = db;
+      next();
+    })
+    .catch(function (err) {
+      connection = undefined;
+      next(err);
+    });
+});
+
+app.use(express.json());
+
 app.use('/students', students);
 app.use('/instructors', instructors);
 app.use('/classes', classes);
