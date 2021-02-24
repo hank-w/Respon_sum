28989170f18804eda6d5c13261c2646c8629e2a5
Ryan Dancy <ryan@keal.ca>
2021-02-23 21:53:57 -0500

    Achievement unlocked: full stack developer

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,26 +2,39 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Student, ID } from './types/api';
-import { getStudentId } from './api/students';
+import { getStudentId, createStudent } from './api/students';
 
 function App() {
   const [studentId, setStudentId] = useState<ID>('');
   const [student, setStudent] = useState<Student>();
 
+  const [postStudent, setPostStudent] = useState<string>('');
+  const [postResult, setPostResult] = useState<string>('');
+
   const onButtonPress = async () => {
-    setStudent(await getStudentId(studentId));
+    setStudent((await getStudentId(studentId)).data);
+  };
+
+  const onPostButtonPress = async () => {
+    console.log(postStudent);
+    console.log(JSON.parse(postStudent));
+    const response = await createStudent(JSON.parse(postStudent));
+    console.log(response);
+    setPostResult(JSON.stringify(response.data));
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload ayyyyy.
         </p>
         <input type="textarea" value={studentId} onChange={e => setStudentId(e.target.value)} />
-        <button onClick={onButtonPress} />
+        <button onClick={onButtonPress}>GET</button>
         <input type="textarea" disabled={true} value={JSON.stringify(student)} />
+        <input type="textarea" value={postStudent} onChange={e => setPostStudent(e.target.value)} />
+        <button onClick={onPostButtonPress}>POST</button>
+        <input type="textarea" disabled={true} value={postResult} />
       </header>
     </div>
   );
--- web/src/api/students.tsx
+++ web/src/api/students.tsx
@@ -3,6 +3,14 @@ import axios from 'axios';
 import { ID, Stats, Student } from '../types/api';
 import { BASE_URL } from './api';
 
-export const getStudentId = async (id: ID): Promise<Student> => {
-  return axios.get(`${BASE_URL}/students/${id}`);
+export const getStudentId = async (id: ID): Promise<any> => {
+  return axios.get(`http://${BASE_URL}/students/${id}`);
+};
+
+export const putStudentId = async (id: ID, student: Student): Promise<any> => {
+  return axios.put(`http://${BASE_URL}/students/${id}`, student);
+};
+
+export const createStudent = async (student: Student): Promise<any> => {
+  return axios.post(`http://${BASE_URL}/students`, student);
 };
