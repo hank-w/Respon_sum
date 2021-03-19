fab95306464d38a09debb0431bef019ef8163c61
Ryan Dancy <ryan@keal.ca>
2021-03-18 22:01:41 -0400

    controlled component

--- web/src/components/pages/StudentLogin.tsx
+++ web/src/components/pages/StudentLogin.tsx
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import { StudentLoginWrapper, LoginButton, SignupButton } from '../style/StudentLogin';
 
 export default () => {
+  const [uuid, setUUID] = useState<string>();
+
   return (
     <StudentLoginWrapper>
       <div style={{display: 'flex', flexDirection: 'column'}}>
         <label>uuid:</label>
-        <input type="text" id="uuid" name="uuidLogin" />
+        <input type="text" id="uuid" name="uuidLogin" value={uuid} onChange={e => setUUID(e.target.value)} />
       </div>
       <LoginButton>
         Login
