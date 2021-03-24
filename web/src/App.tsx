27b688c5318cdbf8dbff429a5e4e4d15010c2674
Hank Wu <hank.j.wu@gmail.com>
2021-03-23 21:43:44 -0400

    cornflake dropshadow achieved

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 import './App.css';
 import { Student, ID } from './types/api';
 import { getStudentId, createStudent } from './api/students';
 import Routes from './Routes';
-import { Header } from './components/Header';
+// import { Header } from './components/Header';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import 'antd/dist/antd.css';
+import Logo from './resources/logo.png';
+import { BASE_PATH } from './utils/Paths';
+const { Header, Content, Footer } = Layout;
 
 function App() {
   const [studentId, setStudentId] = useState<ID>('');
@@ -28,13 +33,27 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Header />
-      <div style={{
-        display: 'flex',
-        justifyContent: 'center',
-      }}>
-        <Routes />
-      </div>
+      <Layout className="layout">
+        <Header style={{ display: 'flex', flexDirection: 'row' }}>
+          <Menu style={{ flexGrow: 1 }} theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+          <div>
+            <Link to={BASE_PATH}>
+              <img src={Logo} style={{ height: '70%', marginRight: 20, filter: 'brightness(100)' }} />
+              <span style={{ color: 'white', fontFamily: 'Helvetica', fontSize: '1.5em' }}>Responsum</span>
+            </Link> 
+          </div>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <div className="site-layout-content">
+            <Routes />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Responsum Design ©2021 Created by Ryan and Hank</Footer>
+      </Layout>
     </BrowserRouter>
   );
 }
