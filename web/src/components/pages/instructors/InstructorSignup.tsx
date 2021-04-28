242343ddf8264059b182570d9f4ce9783ed08273
Hank Wu <hank.j.wu@gmail.com>
2021-04-28 11:35:51 -0400

    added instructor signup file

--- web/src/components/pages/instructors/InstructorSignup.tsx
+++ web/src/components/pages/instructors/InstructorSignup.tsx
@@ -1,3 +1,79 @@
-export default () => {
-  return <></>
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Button, Space, Input, Form, Typography } from 'antd';
+import { createInstructor } from '../../../api/instructors';
+import { INSTRUCTORS_PATH } from '../../../utils/Paths';
+import { useDispatch } from 'react-redux';
+import { Instructor } from '../../../types/api';
+import { setInstructor } from '../../../utils/Actions';
+
+const { Title } = Typography;
+
+const DEFAULT_STUDENT: Instructor = {
+  id: '',
+  name: '',
+  email: '',
+  studentNumber: '',
+  institution: '',
 };
+
+const InstructorSignup = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [creatingInstructor, setCreatingInstructor] = useState(DEFAULT_STUDENT);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const createAccount = () => {
+    setLoading(true);
+    createInstructor(creatingInstructor as Instructor)
+    .then(res => {
+      setError(null);
+      const id = res.data.id + '';
+      alert(`Your UUID is "${id}". Don't lose it! Put it somewhere safe!`);
+      const newInstructor = { ...creatingInstructor, id };
+      dispatch(setInstructor(newInstructor));
+      history.push(INSTRUCTORS_PATH);
+    })
+    .catch(err => {
+      console.log(err);
+      setError(err?.message+'' || err?.msg+'' || err+'');
+    })
+    .finally(() => {
+      setLoading(false);
+    });
+  };
+  
+  const s = (creatingInstructor as Instructor);
+  return (
+    <Space direction="vertical" size="large" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+      <div>
+        <Title level={2}>Sign up</Title>
+        <span style={{color:'red'}}>{error}</span>
+        <Form onFinish={createAccount}>
+          <Form.Item label="Name" required>
+            <Input placeholder="John Smith" value={s.name}
+              onChange={e => setCreatingInstructor({...s, name: e.target.value})} />
+          </Form.Item>
+          <Form.Item label="Email" required >
+            <Input placeholder="jsmith@awesome.edu" value={s.email} 
+              onChange={e => setCreatingInstructor({...s, email: e.target.value})} />
+          </Form.Item>
+          <Form.Item label="Instructor Number" required>
+            <Input placeholder="13377331" value={s.studentNumber}
+              onChange={e => setCreatingInstructor({...s, studentNumber: e.target.value})} />
+          </Form.Item> 
+          <Form.Item label="Institution" required>
+            <Input placeholder="University of Awesome" value={s.institution}
+              onChange={e => setCreatingInstructor({...s, institution: e.target.value})}/>
+          </Form.Item>
+          <Button type="primary" htmlType="submit">
+            Create Account
+          </Button>
+        </Form>
+      </div>
+    </Space>
+  );
+};
+
+export default InstructorSignup;
