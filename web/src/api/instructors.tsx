055300afbca730ab01df9569b6103b60ecfde208
Hank Wu <hank.j.wu@gmail.com>
2021-04-28 11:24:35 -0400

    added epic full stack api calls lol

--- web/src/api/instructors.tsx
+++ web/src/api/instructors.tsx
@@ -2,3 +2,19 @@ import 'axios';
 import axios from 'axios';
 import { ID, Instructor } from '../types/api';
 import { BASE_URL} from './api';
+
+export const getInstructorById = async (id: ID): Promise<any> => {
+  return axios.get(`http://${BASE_URL}/instructors/${id}`);
+};
+
+export const putStudentById = async (id: ID, instructor: Instructor): Promise<any> => {
+    return axios.put(`http://${BASE_URL}/students/${id}`, instructor);
+  };
+  
+  export const deleteStudentById = async (id: ID): Promise<any> => {
+    return axios.delete(`http://${BASE_URL}/students/${id}`);
+  };
+  
+  export const createStudent = async (instructor: Instructor): Promise<any> => {
+    return axios.post(`http://${BASE_URL}/students`, instructor);
+  };
