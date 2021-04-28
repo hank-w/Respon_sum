b258beb9eb091384566740949244f79b89aacc20
Hank Wu <hank.j.wu@gmail.com>
2021-04-28 11:49:39 -0400

    Added instructor homepage

--- web/src/components/pages/instructors/InstructorHomepage.tsx
+++ web/src/components/pages/instructors/InstructorHomepage.tsx
@@ -1,3 +1,43 @@
+import { useState } from 'react';
+import { Layout, Menu } from 'antd';
+import { UserOutlined, QuestionCircleOutlined, BookOutlined } from '@ant-design/icons';
+
+import InstructorQuestionsHome from './InstructorQuestionsHome';
+import InstructorAccountSettings from './InstructorAccountSettings';
+import InstructorClasses from './InstructorClasses';
+
+const { Content, Sider } = Layout;
+ 
 export default () => {
-    return <>Hello</>
-  };
+  const [ page, setPage ] = useState('questions');
+
+  return (
+    <Layout style={{ padding: '24px 0' }}>
+      <Sider width={200}>
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{ height: '100%' }}
+        >
+          <Menu.Item key="1" icon={<QuestionCircleOutlined />} onClick={() => setPage('questions')}>
+            Questions
+          </Menu.Item>
+          <Menu.Item key="2" icon={<BookOutlined />} onClick={() => setPage('classes')}>
+            Classes
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UserOutlined />} onClick={() => setPage('account')}>
+            Account Settings
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Content style={{ padding: '0 24px', minHeight: 280 }}>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          {page === 'questions' ? <InstructorQuestionsHome /> : null}
+          {page === 'classes' ? <InstructorClasses /> : null}
+          {page === 'account' ? <InstructorAccountSettings /> : null}
+        </div>
+      </Content>
+    </Layout>
+  );
+};
